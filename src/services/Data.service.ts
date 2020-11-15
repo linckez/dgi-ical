df59cb9ba28dfe718b81a0197104fd0293e2c26b
linckez <slhlinck@gmail.com>
2020-11-16 00:37:45 +0100

    test3

--- src/services/Data.service.ts
+++ src/services/Data.service.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-unassigned-import
 import 'dotenv/config';
 import axios, { AxiosRequestConfig, AxiosError } from 'axios';
-import { parseString, parseStringPromise } from 'xml2js';
+import { ParserOptions, parseString, parseStringPromise } from 'xml2js';
 import ical from 'ical-generator';
 import moment from 'moment';
 
@@ -24,23 +24,23 @@ export class DataService {
 
     try {
       const request = await (await axios.request(options)).data;
-      const foo: any = await parseStringPromise(request);
+      const jsonData: any = await parseStringPromise(request);
+      const events: any = jsonData.Events.Event;
 
-      const cal = ical({ domain: 'github.com', name: 'my first iCal' });
+      const cal = ical({ domain: 'github.com', name: 'my second iCal' });
 
       cal.createEvent({
-        start: moment(),
-        end: moment().add(1, 'hour'),
-        summary: 'Example Event',
-        description: 'It works ;)',
-        location: 'my room',
-        url: 'http://sebbo.net/',
+        start: '11-17-2020 09:45',
+        end: '11-17-2020 16:00',
+        summary: 'Indgang til DK-CAMP leverand�rmesse',
+        description: 'Ind Vest',
+        location: 'Indgang Vest',
       });
 
       cal.saveSync('foo.ical');
 
       // eslint-disable-next-line no-console
-      console.log(foo.Events);
+      console.log(events);
 
       return 'asdasdasdas';
     } catch (err) {
