f6e5e71c6493924643904df188e51cd8704551a3
linckez <slhlinck@gmail.com>
2020-11-16 00:21:30 +0100

    test

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-/* eslint-disable func-style */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable no-console */
-/* eslint-disable new-cap */
 // eslint-disable-next-line import/no-unassigned-import
 import 'dotenv/config';
 import { DataService } from './services/Data.service';
 
 // Services
-const users = new DataService();
+const data = new DataService();
 
+
+async function foobar() {
+  console.log(await data.foobar());
+}
+foobar();
--- src/services/Data.service.ts
+++ src/services/Data.service.ts
@@ -1,18 +1,50 @@
 // eslint-disable-next-line import/no-unassigned-import
 import 'dotenv/config';
+import axios, { AxiosRequestConfig, AxiosError } from 'axios';
+import { parseString, parseStringPromise } from 'xml2js';
+import ical from 'ical-generator';
+import moment from 'moment';
 
 export class DataService {
-  public async foobar() {
+  public async foobar(): Promise<unknown> {
     try {
       const result = await this.fetchData();
 
+      return result;
     } catch (err) {
       throw err;
     }
   }
 
-  private async fetchData(): Promise<string> {
+  private async fetchData(): Promise<unknown> {
+    const options: AxiosRequestConfig = {
+      method: 'GET',
+      url: 'https://rooms-dgihusetvejle.azurewebsites.net/30.xml',
+    };
 
-    return '';
+    try {
+      const request = await (await axios.request(options)).data;
+      const foo: any = await parseStringPromise(request);
+
+      const cal = ical({ domain: 'github.com', name: 'my first iCal' });
+
+      cal.createEvent({
+        start: moment(),
+        end: moment().add(1, 'hour'),
+        summary: 'Example Event',
+        description: 'It works ;)',
+        location: 'my room',
+        url: 'http://sebbo.net/',
+      });
+
+      cal.saveSync('foo.ical');
+
+      // eslint-disable-next-line no-console
+      console.log(foo.Events);
+
+      return 'asdasdasdas';
+    } catch (err) {
+      throw err;
+    }
   }
 }
